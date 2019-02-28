198f5adee611b91723259a0caf598ab59f0b57ad
Bullcaos <adrian.quigar@gmail.com>
2019-02-28 19:36:34 +0100

    Hash creator completed

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ FILENAME_INPUT = 'input/a_example.txt'
 FILENAME_OUTPUT_BASE = 'output/results'
 
 
-def remove_newline(name):
-    if name[-2:] == '\n':
-        return name[:-2]
-    return name
-
-
 def create_hash(pics):
     result = {}
     i = 0
     while i < len(pics):
         j = 2
         while j < len(pics[i]):
-            result[remove_newline(pics[i][j])] = i
+            if pics[i][j] not in result:
+                result[pics[i][j]] = []
+            result[pics[i][j]].append(i)
             j = j+1
         i=i+1
     return result
@@ -39,7 +35,7 @@ def get_next_common_tag(matrix, id, n, pictures, used):
 def greedy():
     n, pictures, used = read_input(FILENAME_INPUT)
     res = [0]
-    print(res)
+    print(create_hash(pictures))
 
 
 greedy()
