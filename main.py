5bc641c32cada4c699563f340f5cee00041fda91
Bullcaos <adrian.quigar@gmail.com>
2019-02-28 19:28:52 +0100

    Create hash creator

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from stdio import read_input, write_output
 import time
 
-FILENAME_INPUT = 'input/c_memorable_moments.txt'
+FILENAME_INPUT = 'input/a_example.txt'
 FILENAME_OUTPUT_BASE = 'output/results'
+
+
+def remove_newline(name):
+    if name[-2:] == '\n':
+        return name[:-2]
+    return name
+
+
+def create_hash(pics):
+    result = {}
+    i = 0
+    while i < len(pics):
+        j = 2
+        while j < len(pics[i]):
+            result[remove_newline(pics[i][j])] = i
+            j = j+1
+        i=i+1
+    return result
 
 
 def write(matrix):
@@ -12,6 +30,8 @@ def write(matrix):
 
 def greedy():
     n, pictures = read_input(FILENAME_INPUT)
+    #print(pictures)
+    print(create_hash(pictures))
 
 
 greedy()
